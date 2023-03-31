142fc6de5280a303c63d2f84900c0ecf4a6cf634
Syllas Braga <syllasbraga2@gmail.com>
2023-03-31 12:27:42 -0300

    feat: Classes de persistencia

---
+++ src/main/java/com/sb/gerencia/pessoas/repositories/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.sb.gerencia.pessoas.repositories;
+
+import com.sb.gerencia.pessoas.entities.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+}
